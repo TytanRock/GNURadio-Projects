24e7ca7ab6cc825dfa7f7d286931f88f1479f977
TytanRock <nesscory@gmail.com>
2021-03-18 21:02:54 -0400

    Apt_builder now builds the image generated from Apt_Decoder

--- Apt_Builder.py
+++ Apt_Builder.py
@@ -1,4 +1,4 @@
-import sys, time
+import sys, time, socket
 from PyQt6.QtGui import QPixmap, QImage, QColor
 from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel
 from PyQt6.QtCore import QTimer, Qt
@@ -14,12 +14,15 @@ class MainWindow(QMainWindow):
 
         self.image = QImage(2080, 1, QImage.Format.Format_Grayscale8)
         self.label = QLabel(self)
+        self.resize (2080, 500)
     
-    def updateImage(self, x, y):
+    def updateImage(self, x, y, c):
         if not self.lastY == y:
             self.image = self.image.copy(0, 0, 2080, y+1)
             self.lastY = y
-        self.image.setPixelColor(x,y,QColor(128,128,128))
+        self.image.setPixelColor(x,y,QColor(c,c,c))
+    
+    def refreshImage(self):
         pixmap = QPixmap.fromImage(self.image)
         self.label.setPixmap(pixmap)
         self.setCentralWidget(self.label)
@@ -29,20 +32,29 @@ app = QApplication(sys.argv)
 w = MainWindow()
 w.show()
 
-i = 0
-j = 0
+UDP_IP = "127.0.0.1"
+UDP_PORT = 7762
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+x = 0
+y = 0
 def run():
-    global i
-    global j
-    print('Updating ' + str(i) + ' ' + str(j))
-    w.updateImage(i, j)
-    i += 1
-    if i == 2080:
-        i = 0
-        j += 1
+    global x
+    global y
+    global sock
+    global w
+    data, addr = sock.recvfrom(2080)
+    for i in range(len(data)):
+        w.updateImage(x, y, data[i])
+        x += 1
+        if x >= 2080:
+            x = 0
+            y += 1
+    
+    w.refreshImage()
 
 timer = QTimer()
 timer.timeout.connect(run)
-timer.start(10)
+timer.start(1)
 
 sys.exit(app.exec())
